44a88dc94f7056560979d924d1536f21ef593384
Alihan Toygun <dev.toygun@gmail.com>
2026-05-01 01:42:41 +0300

    Add base controller response helpers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
+
 abstract class Controller
 {
-    //
+    /**
+     * Ortak yanıt yapısını üretir.
+     *
+     * @param array<string, mixed> $extra
+     * @return array<string, mixed>
+     */
+    protected function buildResponse(
+        string $type,
+        string $message,
+        bool $status,
+        string $redirect,
+        array $extra = []
+    ): array {
+        return array_merge([
+            'type' => $type,
+            'message' => $message,
+            'status' => $status,
+            'redirect' => $redirect,
+        ], $extra);
+    }
+
+    /**
+     * Blade sayfaları için ortak view yanıtını döndürür.
+     *
+     * @param array<string, mixed> $response
+     * @param array<string, mixed> $data
+     */
+    protected function respondWithView(string $view, array $response, array $data = []): View
+    {
+        return view($view, array_merge($data, [
+            'response' => $response,
+        ]));
+    }
+
+    /**
+     * İşlem tabanlı uçlar için ortak JSON yanıtını döndürür.
+     *
+     * @param array<string, mixed> $response
+     */
+    protected function respondWithJson(array $response): JsonResponse
+    {
+        return response()->json($response);
+    }
 }
